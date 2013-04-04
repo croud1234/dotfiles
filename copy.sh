93b09c4508719499283bd68dfc3c09ee473afd71
croud1234 <tkusu@paperboy.co.jp>
2013-04-04 13:59:32 +0900

    .sshの処理追加

--- copy.sh
+++ copy.sh
@@ -3,6 +3,13 @@ if [ ! -d $HOME/bin ]
 then
     mkdir $HOME/bin
 fi
+
+if [ ! -d $HOME/.ssh ]
+then
+    mkdir $HOME/.ssh
+fi
+
 cp bin/* $HOME/bin
+cp .ssh/* $HOME/.ssh
 
 source $HOME/.bash_profile
